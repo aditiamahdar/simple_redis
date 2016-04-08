386e7d3801f0afc17442687c96e14346b3c89ec9
Aditia Mahdar <adit@41studio.com>
2016-04-08 21:52:59 +0700

    Prepared main function

--- lib/redis_cache.rb
+++ lib/redis_cache.rb
@@ -1,2 +1,10 @@
 module RedisCache
+  # Set or Get cache from Redis, opts => db, key, value, block
+  def self.fetch(opts={})
+    if block_given?
+      # Use yield to set the data
+    else
+      # Use opts[:value] to set the data
+    end
+  end
 end
