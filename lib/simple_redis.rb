80331f17b9fc97aa8a5d593d0e276c0fb1f5835a
Aditia Mahdar <adit@41studio.com>
2016-04-10 07:09:09 +0700

    Set constant for default configuration

--- lib/simple_redis.rb
+++ lib/simple_redis.rb
@@ -43,7 +43,7 @@ module SimpleRedis
     end
 
     def self.get_redis(opts)
-      Redis.new(host: host || 'localhost', port: port || 6379, db: opts[:db] || default_db || 'simple-redis-cache')
+      Redis.new(host: host || HOST, port: port || PORT, db: opts[:db] || default_db || DEFAULT_DB)
     end
 
     def self.get_result(redis_result)
--- lib/simple_redis/version.rb
+++ lib/simple_redis/version.rb
@@ -1,3 +1,6 @@
 module SimpleRedis
-  VERSION = "0.1.5"
+  VERSION = "0.1.7"
+  HOST = "localhost"
+  PORT = 6379
+  DEFAULT_DB = "simple-redis-cache"
 end
